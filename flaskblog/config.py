904946144d6a6495435670424e6d6c377299021d
Mansoor Abbas Ali <mansoorabbasali@Mansoors-MacBook-Pro.local>
2019-11-03 10:45:31 -0600

    initial commit

---
+++ flaskblog/config.py
@@ -0,0 +1,12 @@
+import os
+
+
+class Config:
+
+    SECRET_KEY='472e0b2048f84fbaac1aca8a43b6bffa'
+    SQLALCHEMY_DATABASE_URI='sqlite:///site.db'
+    MAIL_SERVER='smtp.googlemail.com'
+    MAIL_PORT=587
+    MAIL_USE_TLS=True
+    MAIL_USERNAME='mansoorabbas043@gmail.com'
+    MAIL_PASSWORD='elementfire0'
